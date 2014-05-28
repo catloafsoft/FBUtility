1317bb711ced766f5f33e4225d3769d10671ee7b
Stephane Peter <megastep@megastep.org>
2014-05-27 17:58:18 -0700

    Initial Commit

---
+++ FBUtility/FBUtility.h
@@ -0,0 +1,13 @@
+//
+//  FBUtility.h
+//  FBUtility
+//
+//  Created by Stéphane Peter on 5/27/14.
+//  Copyright (c) 2014 Catloaf Software, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FBUtility : NSObject
+
+@end
---
+++ FBUtility/FBUtility.m
@@ -0,0 +1,13 @@
+//
+//  FBUtility.m
+//  FBUtility
+//
+//  Created by Stéphane Peter on 5/27/14.
+//  Copyright (c) 2014 Catloaf Software, LLC. All rights reserved.
+//
+
+#import "FBUtility.h"
+
+@implementation FBUtility
+
+@end
