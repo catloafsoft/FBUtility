d37465af9d4d4f0387e27cd8e316e8d392cdf5d0
Stephane Peter <megastep@megastep.org>
2014-09-10 17:00:48 -0700

    Added a new iOS Framework target.

---
+++ FBUtility/FBUtility.h
@@ -0,0 +1,21 @@
+//
+//  FBUtility.h
+//  FBUtility
+//
+//  Created by Stéphane Peter on 9/10/14.
+//  Copyright (c) 2014 Catloaf Software, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <FBUtility/CLSFBUtility.h>
+
+//! Project version number for FBUtility.
+FOUNDATION_EXPORT double FBUtilityVersionNumber;
+
+//! Project version string for FBUtility.
+FOUNDATION_EXPORT const unsigned char FBUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBUtility/PublicHeader.h>
+
+
