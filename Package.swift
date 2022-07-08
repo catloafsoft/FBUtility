6dc22a937b14866d35b18b88f2464579fd44a6ea
Stephane Peter <megastep@megastep.org>
2022-07-08 03:32:24 -0700

    SwiftPM config

---
+++ Package.swift
@@ -0,0 +1,37 @@
+//
+//  Package.swift
+//  FBUtility
+//
+//  Created by Stéphane Peter on 7/6/22.
+//  Copyright © 2022 Catloaf Software, LLC. All rights reserved.
+//
+
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "FBUtility",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "FBUtility",
+            targets: ["FBUtility"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/facebook/facebook-ios-sdk", branch: "main")
+    ],
+    targets: [
+        .target(
+            name: "FBUtility",
+            path: "FBUtility"
+        ),
+        .testTarget(
+            name: "FBUtilityTests",
+            path: "FBUtilityTests",
+            dependencies: ["FBUtility"]
+        )
+    ]
+)
